b36e21a618a1e7ce669c9cf75b25cae3dad0a3ba
Justin Morrow <justin.parker.morrow@gmail.com>
2025-01-15 21:33:55 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+
+
+// OLD WAY OF DOING MAIN
+//public class App
+//{
+//	public static void Main(string[] args)
+//	{
+//		Console.WriteLine("Hello, World!");
+//	}
+//}
+
+// TOP LEVEL STATEMENTS MMMMMMMMMM...
+
+Console.WriteLine("Leetcode go brrrrrr!");
+
+var twoSum = TwoSum.Go(new[] { 2, 7, 10, 11 }, 9);
+Debug.Assert(twoSum.Length == 2 && twoSum[0] == 0 && twoSum[1] == 1);
---
+++ TwoSum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class TwoSum
+{
+	public static int[] Go(int[] nums, int target)
+	{
+		Dictionary<int, int> ht = new Dictionary<int, int>();
+
+		for (int i = 0; i < nums.Length; i++)
+		{
+			int tmp = nums[i];
+
+			if (ht.ContainsKey(target - tmp))
+			{
+				return new int[] { ht[target - tmp], i };
+			}
+
+			ht[tmp] = i;
+		}
+
+		return new int[0];
+	}
+}
