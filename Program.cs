a248a20f40fbd2dfc30bfb065ecc73f5a0ee72f3
Justin Morrow <justin.parker.morrow@gmail.com>
2025-01-16 19:15:06 -0600

    added valid parentheses balancer

--- Program.cs
+++ Program.cs
@@ -22,3 +22,12 @@ Debug.Assert(palindromeNumber);
 
 var lcprefix = LongestCommonPrefix.Go(new[] { "asser", "assist", "assortment" });
 Debug.Assert(lcprefix == "ass");
+
+var validParenthesses1 = ValidParentheses.Go("(a{b[c]d}e)");
+var validParenthesses2 = ValidParentheses.Go("({[}])");
+var validParenthesses3 = ValidParentheses.Go("]");
+Debug.Assert(validParenthesses1);
+Debug.Assert(!validParenthesses2);
+Debug.Assert(!validParenthesses3);
+
+
---
+++ ValidParentheses.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class ValidParentheses
+{
+	public static bool Go(string s)
+	{
+		/* BAD 13ms
+		var l = new Stack<char>();
+		var validChars = new[] { '(', '{', '[', ')', '}', ']' };
+		for (int i = 0; i < s.Length; i++)
+		{
+			if (!validChars.Any(x => x == s[i])) continue;
+			if (validChars.Take(3).Any(x => x == s[i]))
+			{
+				l.Push(s[i]);
+				continue;
+			}
+			if (l.Count == 0) return false;
+			var idx = validChars.ToList().FindIndex(x => x == s[i]);
+			if (validChars.Any(x => x == s[i]) && validChars[idx - 3] == l.Peek())
+			{
+				l.Pop();
+			}
+			else
+			{
+				return false;
+			}
+		}
+		return l.Count == 0;
+		*/
+
+		Stack<char> stack = new Stack<char>();
+
+		foreach (char ch in s)
+		{
+			if (ch == '(') stack.Push(')');
+			else if (ch == '{') stack.Push('}');
+			else if (ch == '[') stack.Push(']');
+			else
+			{
+				if (stack.Count == 0 || stack.Pop() != ch)
+				{
+					return false;
+				}
+			}
+		}
+
+		return stack.Count == 0;
+	}
+}
